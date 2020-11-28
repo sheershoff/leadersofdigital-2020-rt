ea4b1944ab74138eeebcbbe97f762e8c7af513a3
Ilya Sheershoff <sheershoff@gmail.com>
2020-11-28 21:06:21 +0500

    add browser tts

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,14 +5,64 @@
 $this->title = 'Языковая среда';
 ?>
 <script>
+    var recognition = null;
+    var final_transcript = '';
+    var final_span, interim_span;
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    function linebreak(str) {
+        return str + '\n';
+    }
+
+    function startTalk(event) {
+        recognition.lang = 'en-US';
+        recognition.start();
+    }
+
+    function endTalk(event) {
+        console.debug('end talk');
+        recognition.stop();
+    }
+
     $(document).ready(function () {
-            if (!('webkitSpeechRecognition' in window)) {
-                $('#speech_welcome').html('<div class="warning">Вам нужен браузер с поддержкой Speech Recognition. Например, последний Chromium.</div>');
-            } else {
-                console.debug('has speech')
+
+        if (!('webkitSpeechRecognition' in window)) {
+            $('#speech_welcome').html('<div class="warning">Вам нужен браузер с поддержкой Speech Recognition. Например, последний Chromium.</div>');
+        } else {
+            final_span = document.getElementById('final_span');
+            interim_span = document.getElementById('interim_span');
+
+            recognition = new webkitSpeechRecognition();
+            recognition.continuous = true;
+            recognition.interimResults = true;
+
+            recognition.onstart = function () {
             }
+            recognition.onresult = function (event) {
+            }
+            recognition.onerror = function (event) {
+            }
+            recognition.onend = function () {
+            }
+
+            recognition.onresult = function (event) {
+                var interim_transcript = '';
+                for (var i = event.resultIndex; i < event.results.length; ++i) {
+                    if (event.results[i].isFinal) {
+                        final_transcript += capitalize(event.results[i][0].transcript + '\n');
+                    } else {
+                        interim_transcript += event.results[i][0].transcript;
+                    }
+                }
+                final_transcript = final_transcript;
+                final_span.innerHTML = linebreak(final_transcript);
+                interim_span.innerHTML = linebreak(interim_transcript);
+            };
         }
-    );
+    });
 </script>
 <div class="site-index">
 
@@ -20,54 +70,20 @@ $this->title = 'Языковая среда';
         <h1>Погрузись в языковую среду!</h1>
 
         <p class="lead">Читай книги по ролям с роботом, слушай, говори, в любое время.</p>
-
-        <p id="speech_welcome"><a class="btn btn-lg btn-success" href="/site/login">Начать!</a></p>
     </div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a>
-                </p>
-            </div>
+    <div class="body-content" id="speech_welcome">
+        <div class="row" style="margin-bottom: 30px;">
+            <button class="btn btn-success" id="start_talk"
+                    onmousedown="startTalk();"
+                    onmouseout="endTalk();"
+                    onmouseup="endTalk();">Нажми и говори
+            </button>
         </div>
-
+        <div class="row" id="row_results">
+            <pre id="interim_span"></pre>
+            <hr/>
+            <pre id="final_span"></pre>
+        </div>
     </div>
 </div>
